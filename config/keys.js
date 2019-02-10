c6e744cd3828d92e82be1dad47810389bf95315c
eliotrhys <eliot@eliotshort.co.uk>
2019-02-10 23:44:02 +0000

    working so far apart from date thing

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: 'mongodb://eliotrhys:Abc123!@ds127995.mlab.com:27995/devconnectoreliot'
-}
+  mongoURI: 'mongodb://eliotrhys:Abc123!@ds127995.mlab.com:27995/devconnectoreliot',
+  secretOrKey: 'secret'
+};
